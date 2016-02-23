fdac9ae395ed56938159df45675a25b93503caf7
AlixSperoza <denis.drouin@supinternet.fr>
2016-02-23 11:31:03 +0100

    Add Console

---
+++ Console.php
@@ -0,0 +1,46 @@
+<?php
+    require('vendor/autoload.php');
+    use Console\ORM_Console\Createdb;
+    use Console\ORM_Console\Addentity;
+    use Console\ORM_Console\Updentity;
+    use Console\Bundle\GenerateBundle;
+
+    function read_stdin(){
+        $st = fopen('php://stdin', 'r');
+        $input = fgets($st, 200);
+        $input = rtrim($input);
+        fclose($st);
+
+        return $input;
+    }
+
+    if(count($argv) == 1){
+        echo 'Aucun paramètre'.PHP_EOL;
+        exit();
+    }
+
+    $expl = explode(':', $argv[1]);
+
+    if($expl[0] == 'ORM' && $expl[1] == 'createdb'){
+        new Createdb();
+    }
+
+    if($expl[0] == 'ORM' && $expl[1] == 'addentity' && !empty($expl[2]) && !empty($argv[2])){
+        new Addentity($expl[2], $argv[2]);
+    }
+
+    if($expl[0] == 'ORM' && $expl[1] == 'updentity' && !empty($expl[2])){
+        new Updentity($expl[2]);
+    }
+
+    if($expl[0] == 'Bundle' && $expl[1] == 'generate' && !empty($expl[2])){
+        new GenerateBundle($expl[2]);
+    }
+
+    if($expl[0] == 'console' && $expl[1] == 'listcommand'){
+        require_once('Console/listcommand.php');
+
+        foreach($array as $k => $v){
+            echo $k.' => '.$v.PHP_EOL;
+        }
+    }
