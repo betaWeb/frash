f5933d442d4e8037a60129391ad371acba390f9a
AlixSperoza <denis.drouin@supinternet.fr>
2016-02-23 16:54:45 +0100

    Add ConnexionController

---
+++ Bundles/GameBundle/Controllers/ConnexionController.php
@@ -0,0 +1,30 @@
+<?php
+    namespace Bundles\GameBundle\Controllers;
+    use Composants\Framework\Response\Redirect;
+    use Bundles\GameBundle\Requests\ConnexionRequests;
+
+    class ConnexionController{
+        public function connexionAction(){
+            if(isset($_POST['submit']) && strlen($_POST['pseudo']) <= 40){
+                $pseudo = htmlentities($_POST['pseudo']);
+                $password = sha1($_POST['password']);
+
+                $c = new ConnexionRequests;
+                if($c->sqlVerifPassword($pseudo, $password) === true){
+                    $user = $c->sqlGetInfoUser($pseudo, $password);
+
+                    $_SESSION['id'] = $user['id'];
+                    $_SESSION['pseudo'] = $user['pseudo'];
+                    $_SESSION['terri'] = $c->sqlGetInfoTerri($pseudo);
+
+                    return new Redirect('../game/');
+                }
+                else{
+                    return new Redirect('../accueil/');
+                }
+            }
+            else{
+                return new Redirect('../accueil/');
+            }
+        }
+    }
