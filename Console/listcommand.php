240161d7673bb2da5c762942beef3ef653ecb66c
AlixSperoza <denis.drouin@supinternet.fr>
2016-02-23 11:44:28 +0100

    Add list commands console

---
+++ Console/listcommand.php
@@ -0,0 +1,7 @@
+<?php
+    $array = [
+        'ORM:addentity' => 'Générer une entité et sa table',
+        'ORM:createdb' => 'Créer la base de données',
+        'ORM:updentity' => 'Génère l\'entité de la table sélectionnée',
+        'Bundle:generate' => 'Génère un bundle'
+    ];
