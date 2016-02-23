6329ecbfcf467d3294f80ef6f5dd4ac64cb84e27
AlixSperoza <denis.drouin@supinternet.fr>
2016-02-23 14:56:55 +0100

    Add param to Addentity and Updentity

--- Console.php
+++ Console.php
@@ -25,12 +25,12 @@
         new Createdb();
     }
 
-    if($expl[0] == 'ORM' && $expl[1] == 'addentity' && !empty($expl[2]) && !empty($argv[2])){
-        new Addentity($expl[2], $argv[2]);
+    if($expl[0] == 'ORM' && $expl[1] == 'addentity' && !empty($expl[2]) && !empty($expl[3]) && !empty($argv[2])){
+        new Addentity($expl[2], $expl[3], $argv[2]);
     }
 
-    if($expl[0] == 'ORM' && $expl[1] == 'updentity' && !empty($expl[2])){
-        new Updentity($expl[2]);
+    if($expl[0] == 'ORM' && $expl[1] == 'updentity' && !empty($expl[2]) && !empty($expl[3])){
+        new Updentity($expl[2], $expl[3]);
     }
 
     if($expl[0] == 'Bundle' && $expl[1] == 'generate' && !empty($expl[2])){
