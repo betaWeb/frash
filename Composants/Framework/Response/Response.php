ee93c8e5a9a15b791524d1361bd343c3c59ac45b
AlixSperoza <denis.drouin@supinternet.fr>
2016-02-23 10:27:35 +0100

    Update echo twig => return twig

--- Composants/Framework/Response/Response.php
+++ Composants/Framework/Response/Response.php
@@ -17,12 +17,12 @@
             if($bundle == 'Exception' && file_exists('Composants/Framework/Exception/Views/'.$templ)){
                 $tlf = new \Twig_Loader_Filesystem('Composants/Framework/Exception/Views');
                 $twig = new \Twig_Environment($tlf, [ 'cache' => false ]);
-                echo $twig->render($templ, $param);
+                return $twig->render($templ, $param);
             }
             elseif(file_exists('Bundles/'.$bundle.'/Views/'.$templ)){
                 $tlf = new \Twig_Loader_Filesystem('Bundles/'.$bundle.'/Views');
                 $twig = new \Twig_Environment($tlf, [ 'cache' => false ]);
-                echo $twig->render($templ, $param);
+                return $twig->render($templ, $param);
             }
             else{
                 new TwigChargementTemplateFail($templ);
