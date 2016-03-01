7f23ca7b631784ed52c210734986ae68159ab897
AlixSperoza <denis.drouin@supinternet.fr>
2016-03-01 15:29:24 +0100

    Simplification de Limit

--- Composants/ORM/Request/Limit.php
+++ Composants/ORM/Request/Limit.php
@@ -7,23 +7,12 @@
      */
     class Limit{
         /**
-         * @var
-         */
-        private $limit;
-
-        /**
+         * Limit constructor.
          * @param $limit
          */
         public function __construct($limit){
             if(!is_array($limit)){
-                $this->limit = 'LIMIT '.$limit;
+                return 'LIMIT '.$limit;
             }
-        }
-
-        /**
-         * @return mixed
-         */
-        public function getLimit(){
-            return $this->limit;
         }
     }
