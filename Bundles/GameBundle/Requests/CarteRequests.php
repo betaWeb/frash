aab8ff76049cb1fd819c687ba15fbe1223828cc9
AlixSperoza <denis.drouin@supinternet.fr>
2016-03-02 12:39:43 +0100

    Add commentary to documentation and correction use QB et Where

--- Bundles/GameBundle/Requests/CarteRequests.php
+++ Bundles/GameBundle/Requests/CarteRequests.php
@@ -2,9 +2,19 @@
 	namespace Bundles\GameBundle\Requests;
     use Composants\ORM\Request\Select;
     use Composants\ORM\Request\Where;
-    use Composants\ORM\Request\QueryBuilder;
+    use Composants\ORM\QueryBuilder;
 
+    /**
+     * Class CarteRequests
+     * @package Bundles\GameBundle\Requests
+     */
 	class CarteRequests{
+        /**
+         * @param $pos_x
+         * @param $pos_y_min
+         * @param $pos_y_max
+         * @return array
+         */
 		public function sqlGetLineX($pos_x, $pos_y_min, $pos_y_max){
             $req = new QueryBuilder();
             $sel = new Select('carte');
@@ -12,12 +22,8 @@
             $wh->initNormalWhere([ 'position_x', ':pos_x' ], '=');
             $wh->andWhere([ 'position_y', ':pos_y_min' ], '>');
             $wh->andWhere([ 'position_y', ':pos_y_max' ], '<');
-            $sel->setWhere($wh->getWhere());
-            $sel->setArrayWhere($wh->getArrayWhere());
+            $sel->setWhere($wh->getWhere(), $wh->getArrayWhere());
             $sel->setExecute([ $pos_x, $pos_y_min, $pos_y_max ]);
-            echo '<pre>';
-                print_r($sel->getExecute());
-            echo '</pre>';
             $sel->requestSelect();
             return $req->execRequestSelect($sel->getRequest(), $sel->getExecute(), '\Bundles\GameBundle\Entity\Carte');
         }
