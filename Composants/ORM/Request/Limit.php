e9067212fba8312767b21ce530ee087747a573ae
AlixSperoza <denis.drouin@supinternet.fr>
2016-03-02 11:39:06 +0100

    Add getLimit

--- Composants/ORM/Request/Limit.php
+++ Composants/ORM/Request/Limit.php
@@ -7,12 +7,24 @@
      */
     class Limit{
         /**
+         * @var
+         */
+        private $limit;
+
+        /**
          * Limit constructor.
          * @param $limit
          */
         public function __construct($limit){
             if(!is_array($limit)){
-                return 'LIMIT '.$limit;
+                $this->limit = $limit;
             }
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getLimit(){
+            return $this->limit;
         }
     }
