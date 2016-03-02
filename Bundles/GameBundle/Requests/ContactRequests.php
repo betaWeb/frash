b3ce137cb966ec320faf7ee24431a8ab8354b57d
AlixSperoza <denis.drouin@supinternet.fr>
2016-03-02 12:37:13 +0100

    Add commentary to documentation

--- Bundles/GameBundle/Requests/ContactRequests.php
+++ Bundles/GameBundle/Requests/ContactRequests.php
@@ -3,7 +3,14 @@
     use Composants\ORM\Request\Insert;
     use Composants\ORM\QueryBuilder;
 
+    /**
+     * Class ContactRequests
+     * @package Bundles\GameBundle\Requests
+     */
 	class ContactRequests{
+        /**
+         * @param $text
+         */
 		public function sqlInsertContact($text){
             $req = new QueryBuilder();
             $ins = new Insert('contact');
