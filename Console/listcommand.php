7624ba6beb8ec58353651ff00784e16a09cefa46
AlixSperoza <denis.drouin@supinternet.fr>
2016-03-02 11:07:17 +0100

    Déplacement du dossier console

--- Console/listcommand.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $array = [
-        'ORM:addentity' => 'Generer une entite et sa table',
-        'ORM:createdb' => 'Creer la base de donnees',
-        'ORM:updentity' => 'Generer l\'entite de la table selectionnee',
-        'Bundle:generate' => 'Generer un bundle'
-    ];
