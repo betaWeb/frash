2f822bd36761fe71a429bcc198feddf250dcf9f0
AlixSperoza <denis.drouin@supinternet.fr>
2016-03-07 13:58:36 +0100

    Update AdminController

--- Bundles/GameBundle/Controllers/AdminController.php
+++ Bundles/GameBundle/Controllers/AdminController.php
@@ -15,17 +15,12 @@
          */
         public function adminAction(){
             if(!isset($_SESSION['id']) || !isset($_SESSION['pseudo']) || !isset($_SESSION['terri'])){ return new Redirect('../accueil/'); }
-            
-            $req = new QueryBuilder();
-            $data = $req->findAll('contact', 'GameBundle');
 
             $met = new MenuRequests;
             $user = $met->sqlGetInfoUser();
             $terri = $met->sqlGetInfoTerri();
-
-            return new Response('admin.html', 'GameBundle', [ 
-                'data' => $data, 'rang' => $user['rang'], 'nbmp' => $met->sqlCountMP(), 'nb_monnaie' => $terri['nb_monnaie'], 'nb_uranium' => $terri['nb_uranium'],
-                'nb_acier' => $terri['nb_acier'], 'nb_petrole' => $terri['nb_petrole'], 'nb_composant' => $terri['nb_composant']
-            ]);
+            
+            return new Response('admin.html.twig', 'GameBundle', [ 'rang' => $user['rang'], 'nbmp' => $met->sqlCountMP(), 'nb_monnaie' => $terri['nb_monnaie'],
+                'nb_uranium' => $terri['nb_uranium'], 'nb_acier' => $terri['nb_acier'], 'nb_petrole' => $terri['nb_petrole'], 'nb_composant' => $terri['nb_composant'] ]);
         }
     }
