5a7ef41e78c547658e1a11c76dd4b7298ff8755d
AlixSperoza <denis.drouin@supinternet.fr>
2016-03-17 16:55:35 +0100

    Finalisation de TechnoRequests

--- Bundles/GameBundle/Requests/TechnoRequests.php
+++ Bundles/GameBundle/Requests/TechnoRequests.php
@@ -79,6 +79,10 @@
             }
         }
 
+        /**
+         * @param $joueur
+         * @return array
+         */
         public function sqlGetLevelsRech($joueur){
             $req = new QueryBuilder();
             $sel = new Select('recherche');
@@ -95,5 +99,153 @@
                     'canon' => $v->getCanon_electro(), 'syst_tir' => $v->getSysteme_tir_deporte(), 'aesa' => $v->getRadar_aesa(), 'dhale' => $v->getDrone_hale(), 'vtol' => $v->getReacteur_vtol()
                 ];
             }
+        }
+
+        /**
+         * @param $nrech
+         * @param $joueur
+         * @param $time
+         * @param $temps_fin
+         */
+        public function sqlInsertConstRech($nrech, $joueur, $time, $temps_fin){
+            $req = new QueryBuilder();
+            $ins = new Insert('construct_rech');
+            $ins->setInsert([ 'rech', 'joueur', 'time', 'temps_fin' ]);
+            $ins->setExecute([ $nrech, $joueur, $time, $temps_fin ]);
+            $req->insert($ins->getRequest(), $ins->getExecute());
+        }
+
+        /**
+         * @param $name
+         * @param $joueur
+         */
+        public function sqlUpdateRechTable($name, $joueur){
+            $req = new QueryBuilder();
+            $upd = new Update('recherche');
+            $wh = new Where;
+            $upd->setUpdate([ $name ]);
+            $wh->initNormalWhere('joueur', '=');
+            $upd->setWhere($wh->getWhere(), $wh->getArrayWhere());
+            $upd->setExecute([ 1, $joueur ]);
+            $req->update($upd->getRequest(), $upd->getExecute());
+        }
+
+        /**
+         * @param $monn
+         * @param $ura
+         * @param $acier
+         * @param $comp
+         * @param $terri
+         */
+        public function sqlUpdTerriMUAC($monn, $ura, $acier, $comp, $terri){
+            $req = new QueryBuilder();
+            $upd = new Update('territoire');
+            $wh = new Where;
+            $upd->setUpdate([ 'nombre_monnaie', 'nombre_uranium', 'nombre_acier', 'nombre_composant' ]);
+            $wh->initNormalWhere('id', '=');
+            $upd->setWhere($wh->getWhere(), $wh->getArrayWhere());
+            $upd->setExecute([ $monn, $ura, $acier, $comp, $terri ]);
+            $req->update($upd->getRequest(), $upd->getExecute());
+        }
+
+        /**
+         * @param $monn
+         * @param $ura
+         * @param $acier
+         * @param $petr
+         * @param $comp
+         * @param $terri
+         */
+        public function sqlUpdTerriMUAPC($monn, $ura, $acier, $petr, $comp, $terri){
+            $req = new QueryBuilder();
+            $upd = new Update('territoire');
+            $wh = new Where;
+            $upd->setUpdate([ 'nombre_monnaie', 'nombre_uranium', 'nombre_acier', 'nombre_petrole', 'nombre_composant' ]);
+            $wh->initNormalWhere('id', '=');
+            $upd->setWhere($wh->getWhere(), $wh->getArrayWhere());
+            $upd->setExecute([ $monn, $ura, $acier, $petr, $comp, $terri ]);
+            $req->update($upd->getRequest(), $upd->getExecute());
+        }
+
+        /**
+         * @param $monn
+         * @param $acier
+         * @param $petr
+         * @param $comp
+         * @param $terri
+         */
+        public function sqlUpdTerriMAPC($monn, $acier, $petr, $comp, $terri){
+            $req = new QueryBuilder();
+            $upd = new Update('territoire');
+            $wh = new Where;
+            $upd->setUpdate([ 'nombre_monnaie', 'nombre_acier', 'nombre_petrole', 'nombre_composant' ]);
+            $wh->initNormalWhere('id', '=');
+            $upd->setWhere($wh->getWhere(), $wh->getArrayWhere());
+            $upd->setExecute([ $monn, $acier, $petr, $comp, $terri ]);
+            $req->update($upd->getRequest(), $upd->getExecute());
+        }
+
+        /**
+         * @param $monn
+         * @param $acier
+         * @param $terri
+         */
+        public function sqlUpdTerriMA($monn, $acier, $terri){
+            $req = new QueryBuilder();
+            $upd = new Update('territoire');
+            $wh = new Where;
+            $upd->setUpdate([ 'nombre_monnaie', 'nombre_acier' ]);
+            $wh->initNormalWhere('id', '=');
+            $upd->setWhere($wh->getWhere(), $wh->getArrayWhere());
+            $upd->setExecute([ $monn, $acier, $terri ]);
+            $req->update($upd->getRequest(), $upd->getExecute());
+        }
+
+        /**
+         * @param $monn
+         * @param $acier
+         * @param $comp
+         * @param $terri
+         */
+        public function sqlUpdTerriMAC($monn, $acier, $comp, $terri){
+            $req = new QueryBuilder();
+            $upd = new Update('territoire');
+            $wh = new Where;
+            $upd->setUpdate([ 'nombre_monnaie', 'nombre_acier', 'nombre_composant' ]);
+            $wh->initNormalWhere('id', '=');
+            $upd->setWhere($wh->getWhere(), $wh->getArrayWhere());
+            $upd->setExecute([ $monn, $acier, $comp, $terri ]);
+            $req->update($upd->getRequest(), $upd->getExecute());
+        }
+
+        /**
+         * @param $monn
+         * @param $comp
+         * @param $terri
+         */
+        public function sqlUpdTerriMC($monn, $comp, $terri){
+            $req = new QueryBuilder();
+            $upd = new Update('territoire');
+            $wh = new Where;
+            $upd->setUpdate([ 'nombre_monnaie', 'nombre_composant' ]);
+            $wh->initNormalWhere('id', '=');
+            $upd->setWhere($wh->getWhere(), $wh->getArrayWhere());
+            $upd->setExecute([ $monn, $comp, $terri ]);
+            $req->update($upd->getRequest(), $upd->getExecute());
+        }
+
+        /**
+         * @param $user
+         * @param $point
+         */
+        public function sqlUpdatePointUser($user, $point){
+            $req = new QueryBuilder();
+            $upd = new Update('user');
+            $wh = new Where;
+            $upd->setUpdate([ 'point' ]);
+            $wh->initNormalWhere('id', '=');
+            $upd->setWhere($wh->getWhere(), $wh->getArrayWhere());
+            $upd->setExecute([ $point, $user ]);
+            $req->update($upd->getRequest(), $upd->getExecute());
         }
     }
