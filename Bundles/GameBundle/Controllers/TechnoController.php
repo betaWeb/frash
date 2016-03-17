b0b0c09a9ab1c81ed32c845d9009930820954539
AlixSperoza <denis.drouin@supinternet.fr>
2016-03-17 16:55:26 +0100

    Finalisation de TechnoController

--- Bundles/GameBundle/Controllers/TechnoController.php
+++ Bundles/GameBundle/Controllers/TechnoController.php
@@ -12,6 +12,9 @@
      * @package Bundles\GameBundle\Controllers
      */
     class TechnoController{
+        /**
+         * @return Redirect|Response
+         */
         public function technoAction(){
             if(!isset($_SESSION['id']) || !isset($_SESSION['pseudo']) || !isset($_SESSION['terri'])){ return new Redirect('../accueil/'); }
 
@@ -62,6 +65,114 @@
                         'end' => $form->endForm()
                     ]
                 ]);
+            }
+        }
+
+        /**
+         * @return Redirect
+         */
+        public function technoConfAction(){
+            if(isset($_POST['submit']) && isset($_POST['rech'])){
+                $tr = new TechnoRequests;
+                $met = new MenuRequests;
+                $terri = $met->sqlGetInfoTerri();
+
+                if($_POST['rech'] == 'maniement_uranium' && 290000000 < $terri['dep_monnaie'] && 2000 < $terri['dep_uranium'] && 60000 < $terri['dep_acier'] && 20000 < $terri['dep_composant']){
+                    $new_nb_monnaie = $terri['dep_monnaie'] - 290000000;
+                    $new_nb_uranium = $terri['dep_uranium'] - 2000;
+                    $new_nb_acier = $terri['dep_acier'] - 60000;
+                    $new_nb_comp = $terri['dep_composant'] - 20000;
+
+                    $tr->sqlInsertConstRech(1, $_SESSION['id'], 400, time() + 400);
+                    $tr->sqlUpdateRechTable($_POST['rech'], $_SESSION['id']);
+                    $tr->sqlUpdTerriMUAC($new_nb_monnaie, $new_nb_uranium, $new_nb_acier, $new_nb_comp, $_SESSION['terri']);
+                    $tr->sqlUpdatePointUser($_SESSION['id'], 280);
+                }
+                elseif($_POST['rech'] == 'unites_navales' && 600000000 < $terri['dep_monnaie'] && 7000 < $terri['dep_uranium'] && 90000 < $terri['dep_acier'] && 50000 < $terri['dep_petrole'] && 70000 < $terri['dep_composant']){
+                    $new_nb_monnaie = $terri['dep_monnaie'] - 600000000;
+                    $new_nb_uranium = $terri['dep_uranium'] - 7000;
+                    $new_nb_acier = $terri['dep_acier'] - 90000;
+                    $new_nb_petrole = $terri['dep_petrole'] - 50000;
+                    $new_nb_comp = $terri['dep_composant'] - 70000;
+
+                    $tr->sqlInsertConstRech(2, $_SESSION['id'], 503, time() + 503);
+                    $tr->sqlUpdateRechTable($_POST['rech'], $_SESSION['id']);
+                    $tr->sqlUpdTerriMUAPC($new_nb_monnaie, $new_nb_uranium, $new_nb_acier, $new_nb_petrole, $new_nb_comp, $_SESSION['terri']);
+                    $tr->sqlUpdatePointUser($_SESSION['id'], 320);
+                }
+                elseif($_POST['rech'] == 'voilure_tournante' && 350000000 < $terri['dep_monnaie'] && 20000 < $terri['dep_acier'] && 20000 < $terri['dep_petrole'] && 40000 < $terri['dep_composant']){
+                    $new_nb_monnaie = $terri['dep_monnaie'] - 350000000;
+                    $new_nb_acier = $terri['dep_acier'] - 20000;
+                    $new_nb_petrole = $terri['dep_petrole'] - 20000;
+                    $new_nb_comp = $terri['dep_composant'] - 40000;
+
+                    $tr->sqlInsertConstRech(3, $_SESSION['id'], 470, time() + 470);
+                    $tr->sqlUpdateRechTable($_POST['rech'], $_SESSION['id']);
+                    $tr->sqlUpdTerriMAPC($new_nb_monnaie, $new_nb_acier, $new_nb_petrole, $new_nb_comp, $_SESSION['terri']);
+                    $tr->sqlUpdatePointUser($_SESSION['id'], 300);
+                }
+                elseif($_POST['rech'] == 'bouclier_tourelle' && 30000000 < $terri['dep_monnaie'] && 5000 < $terri['dep_acier']){
+                    $tr->sqlInsertConstRech(4, $_SESSION['id'], 347, time() + 347);
+                    $tr->sqlUpdateRechTable($_POST['rech'], $_SESSION['id']);
+                    $tr->sqlUpdTerriMA($terri['dep_monnaie'] - 30000000, $terri['dep_acier'] - 5000, $_SESSION['terri']);
+                    $tr->sqlUpdatePointUser($_SESSION['id'], 180);
+                }
+                elseif($_POST['rech'] == 'furtivite' && 300000000 < $terri['dep_monnaie'] && 10000 < $terri['dep_composant']){
+                    $tr->sqlInsertConstRech(5, $_SESSION['id'], 450, time() + 450);
+                    $tr->sqlUpdateRechTable($_POST['rech'], $_SESSION['id']);
+                    $tr->sqlUpdTerriMC($terri['dep_monnaie'] - 300000000, $terri['dep_composant'] - 10000, $_SESSION['terri']);
+                    $tr->sqlUpdatePointUser($_SESSION['id'], 310);
+                }
+                elseif($_POST['rech'] == 'grille_anti_roq' && 70000000 < $terri['dep_monnaie'] && 5000 < $terri['dep_composant']){
+                    $tr->sqlInsertConstRech(6, $_SESSION['id'], 301, time() + 301);
+                    $tr->sqlUpdateRechTable($_POST['rech'], $_SESSION['id']);
+                    $tr->sqlUpdTerriMC($terri['dep_monnaie'] - 70000000, $terri['dep_composant'] - 5000, $_SESSION['terri']);
+                    $tr->sqlUpdatePointUser($_SESSION['id'], 195);
+                }
+                elseif($_POST['rech'] == 'mitrailleuse_teleop' && 130000000 < $terri['dep_monnaie'] && 5000 < $terri['dep_acier'] && 15000 < $terri['dep_composant']){
+                    $new_nb_monnaie = $terri['dep_monnaie'] - 130000000;
+                    $new_nb_acier = $terri['dep_acier'] - 5000;
+                    $new_nb_comp = $terri['dep_composant'] - 15000;
+
+                    $tr->sqlInsertConstRech(7, $_SESSION['id'], 413, time() + 413);
+                    $tr->sqlUpdateRechTable($_POST['rech'], $_SESSION['id']);
+                    $tr->sqlUpdTerriMAC($new_nb_monnaie, $new_nb_acier, $new_nb_comp, $_SESSION['terri']);
+                    $tr->sqlUpdatePointUser($_SESSION['id'], 260);
+                }
+                elseif($_POST['rech'] == 'systeme_tir_deporte' && 110000000 < $terri['dep_monnaie'] && 5000 < $terri['dep_composant']){
+                    $tr->sqlInsertConstRech(8, $_SESSION['id'], 397, time() + 397);
+                    $tr->sqlUpdateRechTable($_POST['rech'], $_SESSION['id']);
+                    $tr->sqlUpdTerriMC($terri['dep_monnaie'] - 110000000, $terri['dep_composant'] - 5000, $_SESSION['terri']);
+                    $tr->sqlUpdatePointUser($_SESSION['id'], 245);
+                }
+                elseif($_POST['rech'] == 'canon_electro' && 480000000 < $terri['dep_monnaie'] && 15000 < $terri['dep_acier'] && 100000 < $terri['dep_composant']){
+                    $new_nb_monnaie = $terri['dep_monnaie'] - 480000000;
+                    $new_nb_acier = $terri['dep_acier'] - 15000;
+                    $new_nb_comp = $terri['dep_composant'] - 100000;
+
+                    $tr->sqlInsertConstRech(9, $_SESSION['id'], 682, time() + 682);
+                    $tr->sqlUpdateRechTable($_POST['rech'], $_SESSION['id']);
+                    $tr->sqlUpdTerriMAC($new_nb_monnaie, $new_nb_acier, $new_nb_comp, $_SESSION['terri']);
+                    $tr->sqlUpdatePointUser($_SESSION['id'], 420);
+                }
+                elseif($_POST['rech'] == 'aeronavale' && 250000000 < $terri['dep_monnaie'] && 9000 < $terri['dep_acier'] && 80000 < $terri['dep_composant']){
+                    $new_nb_monnaie = $terri['dep_monnaie'] - 250000000;
+                    $new_nb_acier = $terri['dep_acier'] - 9000;
+                    $new_nb_comp = $terri['dep_composant'] - 80000;
+
+                    $tr->sqlInsertConstRech(10, $_SESSION['id'], 518, time() + 518);
+                    $tr->sqlUpdateRechTable($_POST['rech'], $_SESSION['id']);
+                    $tr->sqlUpdTerriMAC($new_nb_monnaie, $new_nb_acier, $new_nb_comp, $_SESSION['terri']);
+                    $tr->sqlUpdatePointUser($_SESSION['id'], 317);
+                }
+                elseif($_POST['rech'] == 'radar_aesa' && 320000000 < $terri['dep_monnaie'] && 110000 < $terri['dep_composant']){
+                    $tr->sqlInsertConstRech(11, $_SESSION['id'], 667, time() + 667);
+                    $tr->sqlUpdateRechTable($_POST['rech'], $_SESSION['id']);
+                    $tr->sqlUpdTerriMC($terri['dep_monnaie'] - 320000000, $terri['dep_composant'] - 110000, $_SESSION['terri']);
+                    $tr->sqlUpdatePointUser($_SESSION['id'], 398);
+                }
+
+                return new Redirect('../techno/');
             }
         }
     }
