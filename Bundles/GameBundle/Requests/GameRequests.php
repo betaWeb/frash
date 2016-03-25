22bba798e7789e426ebd1f5a38cd876b7499b80d
AlixSperoza <denis.drouin@supinternet.fr>
2016-03-25 12:29:46 +0100

    Remove Order

--- Bundles/GameBundle/Requests/GameRequests.php
+++ Bundles/GameBundle/Requests/GameRequests.php
@@ -1,10 +1,8 @@
 <?php
 	namespace Bundles\GameBundle\Requests;
     use Composants\Framework\ORM\MySQL\QueryBuilder;
-    use Composants\Framework\ORM\MySQL\Request\Order;
     use Composants\Framework\ORM\MySQL\Request\Select;
     use Composants\Framework\ORM\MySQL\Request\Where;
-    use Composants\Framework\ORM\MySQL\Request\Limit;
 
     /**
      * Class GameRequests
@@ -18,10 +16,9 @@
             $req = new QueryBuilder();
             $sel = new Select('topic');
             $wh = new Where;
-            $ord = new Order('time_creation', 'DESC');
             $wh->initNormalWhere('cat', '=');
             $sel->setWhere($wh->getWhere(), $wh->getArrayWhere());
-            $sel->setOrder($ord->getOrder());
+            $sel->setOrder('time_creation DESC');
             $sel->setExecute([ 1 ]);
             $data = $req->select($sel->getRequest(), $sel->getExecute(), 'Topic', 'GameBundle');
 
@@ -34,15 +31,12 @@
          * @return array
          */
         public function sqlGetClassement(){
-            $data = [];
             $rang = 1;
 
             $req = new QueryBuilder();
             $sel = new Select('user');
-            $lim = new Limit('0, 20');
-            $ord = new Order('point', 'DESC');
-            $sel->setOrder($ord->getOrder());
-            $sel->setLimit($lim->getLimit());
+            $sel->setOrder('point DESC');
+            $sel->setLimit('0, 20');
             $sel->setExecute();
             $datat = $req->select($sel->getRequest(), $sel->getExecute(), 'User', 'GameBundle');
 
