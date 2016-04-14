0d1fa1887c617e62d405027ff7a9d4c59bf4de46
AlixSperoza <denis.drouin@supinternet.fr>
2016-04-14 19:25:26 +0200

    Add class TradFr to traductions

---
+++ Composants/Framework/Utility/Traductions/TradFr.php
@@ -0,0 +1,10 @@
+<?php
+    namespace Composants\Framework\Utility\Traductions;
+
+    /**
+     * Class TradFr
+     * @package Composants\Framework\Utility\Traductions
+     */
+    class TradFr{
+        // const NOM = value;
+    }
