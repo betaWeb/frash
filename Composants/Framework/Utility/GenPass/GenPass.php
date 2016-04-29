ac4c2d730c45048f225be78240cb3f51d7af5b12
AlixSperoza <denis.drouin@supinternet.fr>
2016-04-30 00:10:42 +0200

    Modification GenPass

--- Composants/Framework/Utility/GenPass/GenPass.php
+++ Composants/Framework/Utility/GenPass/GenPass.php
@@ -51,32 +51,32 @@
          * @return string
          */
         public function getGenPass(){
-            $array = [];
+            $caract = '';
             $poss = 0;
             if($this->number === true){
-                $array[] = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+                $caract .= '123456789';
                 $poss += 9;
             }
 
             if($this->min === true){
-                $array[] = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+                $caract .= 'abcdefghijklmnopqrstuvwxyz';
                 $poss += 26;
             }
 
             if($this->maj === true){
-                $array[] = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
+                $caract .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
                 $poss += 26;
             }
 
             if($this->otca === true){
-                $array[] = [ '&', '#', '{', '(', '[', '-', '_', '/', '@', ')', ']', '=', '}', '+', '$', '§', '?' ];
+                $caract .= '&#{([-_/@)]=}+$§?';
                 $poss += 17;
             }
 
             $gener = '';
             for($i = 0; $i <= $this->size; $i++){
-                $rand = rand(0, $poss - 1);
-                $gener .= $array[ $rand ];
+                $rand = mt_rand(0, $poss - 1);
+                $gener .= $caract[ $rand ];
             }
 
             return $gener;
