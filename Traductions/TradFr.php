667ea0d9e2fc3156777afa75c1b767b7cd43a4e4
AlixSperoza <denis.drouin@supinternet.fr>
2016-04-29 11:49:43 +0200

    Add example of traduction

---
+++ Traductions/TradFr.php
@@ -0,0 +1,11 @@
+<?php
+    namespace Traductions;
+
+    /**
+     * Class TradFr
+     * @package Traductions
+     */
+    class TradFr{
+        // const NOM = value;
+        // public function showConst(){ echo self::NOM; }
+    }
