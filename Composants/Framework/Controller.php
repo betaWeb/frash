7efc49d03c5362ae16b381fa787b9b09c72587d0
AlixSperoza <denis.drouin@supinternet.fr>
2016-05-02 15:47:54 +0200

    Move function getUrl in Controller

--- Composants/Framework/Controller.php
+++ Composants/Framework/Controller.php
@@ -97,4 +97,30 @@
 
             return true;
         }
+
+        /**
+         * @param $url
+         * @return string
+         */
+        public function getUrl($url){
+            $yaml = Yaml::parse(file_get_contents('Others/config/config.yml'));
+            $nurl = explode('/', ltrim($_SERVER['REQUEST_URI'], '/'));
+
+            if($yaml['env'] == 'local'){
+                if($yaml['traduction']['activate'] == 'yes' && in_array($nurl[1], $yaml['traduction']['available'])){
+                    return '/'.$nurl[0].'/'.$nurl[1].'/'.$url;
+                }
+                else{
+                    return '/'.$nurl[0].'/'.$url;
+                }
+            }
+            elseif($yaml['env'] == 'prod'){
+                if($yaml['traduction']['activate'] == 'yes' && in_array($nurl[0], $yaml['traduction']['available'])){
+                    return '/'.$nurl[0].'/'.$url;
+                }
+                else{
+                    return '/'.$url;
+                }
+            }
+        }
     }
