7ae3f5ec1f0dd7ae2aefb9b20c6f6bf2e03b3459
AlixSperoza <denis.drouin@supinternet.fr>
2016-05-12 11:01:21 +0200

    Add command ORM:addjoin

--- Composants/Console/listcommand.php
+++ Composants/Console/listcommand.php
@@ -1,6 +1,7 @@
 <?php
     $array = [
         'ORM:addentity:bundle:table     ' => 'Générer une entité et sa table',
+        'ORM:addjoin:bundle:nom         ' => 'Générer une entité servant de jointure',
         'ORM:createdb                   ' => 'Créer la base de données',
         'ORM:updentity:bundle:table     ' => 'Générer l\'entité de la table sélectionnée',
         'Bundle:generate:nombundle      ' => 'Générer un bundle'
