fc2a199786ebef51911635666b20da91cbedc705
AlixSperoza <denis.drouin@supinternet.fr>
2016-05-12 12:06:38 +0200

    Add command to generate Controller

--- Composants/Console/listcommand.php
+++ Composants/Console/listcommand.php
@@ -4,5 +4,6 @@
         'ORM:addjoin:bundle:nom         ' => 'Générer une entité servant de jointure',
         'ORM:createdb                   ' => 'Créer la base de données',
         'ORM:updentity:bundle:table     ' => 'Générer l\'entité de la table sélectionnée',
-        'Bundle:generate:nombundle      ' => 'Générer un bundle'
+        'Bundle:generate:nombundle      ' => 'Générer un bundle',
+        'Controller:generate            ' => 'Générer un controller'
     ];
