5cc9d6df7c1910c2e1ab38769ea7803ccdbe2a7a
AlixSperoza <denis.drouin@supinternet.fr>
2016-05-14 09:50:32 +0200

    Add connexion in PostGreSQL

--- Composants/Framework/ORM/Orm.php
+++ Composants/Framework/ORM/Orm.php
@@ -28,6 +28,10 @@
                 if($conn['system'] == 'MySQL'){
                     self::$connexion = new \PDO('mysql:host='.$host.';dbname='.$db.';charset=UTF8;', $user, $password, [ \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC ]);
                 }
+                elseif($conn['system'] == 'PGSQL'){
+                    self::$connexion = new \PDO('pgsql:dbname='.$db.';host='.$host, $user, $password);
+                    self::$connexion->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                }
             }
             catch(\Exception $e){
                 new CreateErrorLog($e->getMessage());
