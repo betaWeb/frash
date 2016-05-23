7c6ec9d32399b32ec91bc36c5a24db938afce510
AlixSperoza <denis.drouin@supinternet.fr>
2016-05-23 12:00:25 +0200

    Update list commands

--- Composants/Console/listcommand.php
+++ Composants/Console/listcommand.php
@@ -1,9 +1,7 @@
 <?php
     $array = [
-        'ORM:addentity:bundle:table     ' => 'Générer une entité et sa table',
-        'ORM:addjoin:bundle:nom         ' => 'Générer une entité servant de jointure',
+        'ORM:addentity:bundle:table     ' => 'Générer une entité',
         'ORM:createdb                   ' => 'Créer la base de données',
-        'ORM:updentity:bundle:table     ' => 'Générer l\'entité de la table sélectionnée',
         'Bundle:generate:nombundle      ' => 'Générer un bundle',
         'Controller:generate            ' => 'Générer un controller'
     ];
