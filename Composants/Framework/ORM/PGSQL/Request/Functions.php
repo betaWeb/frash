5f46bd1cda8839173936a389db5c5772657c0c24
AlixSperoza <denis.drouin@supinternet.fr>
2016-06-22 11:16:07 +0200

    Add class PGSQL\Functions

---
+++ Composants/Framework/ORM/PGSQL/Request/Functions.php
@@ -0,0 +1,31 @@
+<?php
+    namespace Composants\Framework\ORM\PGSQL\Request;
+
+    /**
+     * Class Functions
+     * @package Composants\Framework\ORM\PGSQL\Request
+     */
+    class Functions{
+        /**
+         * @param mixed $pdo
+         * @param array $array
+         * @return string
+         */
+        public function concat($pdo, $array){
+            $arr = [];
+
+            foreach($array as $v){
+                if(substr($v, 0, 2) == 'c '){
+                    $arr[] = substr($v, 2);
+                }
+                elseif(substr($v, 0, 1) == ':'){
+                    $arr[] = "$v::text";
+                }
+                else{
+                    $arr[] = $pdo->quote($v);
+                }
+            }
+
+            return 'CONCAT('.implode(', ', $arr).')';
+        }
+    }
