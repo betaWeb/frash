a7160ba30efe432f50c6746e129cddd493a4a278
AlixSperoza <denis.drouin@supinternet.fr>
2016-06-23 14:02:09 +0200

    Update ControllerChargementFail

--- Composants/Framework/Exception/ControllerChargementFail.php
+++ Composants/Framework/Exception/ControllerChargementFail.php
@@ -2,7 +2,6 @@
     namespace Composants\Framework\Exception;
     use Composants\Framework\Controller;
     use Composants\Framework\CreateLog\CreateErrorLog;
-    use Composants\Yaml\Yaml;
 
     /**
      * Class ControllerChargementFail
@@ -14,13 +13,8 @@
          * @param $controller
          */
         public function __construct($controller){
-            $yaml = Yaml::parse(file_get_contents('Others/config/config.yml'));
+            new CreateErrorLog('Controller '.$controller.' Not Found');
 
-            if($yaml['log']['error'] == 'yes'){
-                new CreateErrorLog('Controller '.$controller.' Not Found');
-            }
-
-            header("HTTP/1.0 404 Not Found");
             return $this->view('ControllerNotFound.html.twig', 'Exception', [ 'controller' => $controller ]);
         }
     }
