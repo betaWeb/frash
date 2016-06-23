7ca5f77eb4e0b617e34663ca1478ff7d9976bd14
AlixSperoza <denis.drouin@supinternet.fr>
2016-06-23 14:01:38 +0200

    Update ActionChargementFail

--- Composants/Framework/Exception/ActionChargementFail.php
+++ Composants/Framework/Exception/ActionChargementFail.php
@@ -2,7 +2,6 @@
     namespace Composants\Framework\Exception;
     use Composants\Framework\Controller;
     use Composants\Framework\CreateLog\CreateErrorLog;
-    use Composants\Yaml\Yaml;
 
     /**
      * Class ActionChargementFail
@@ -11,16 +10,11 @@
     class ActionChargementFail extends Controller{
         /**
          * ActionChargementFail constructor.
-         * @param $action
+         * @param string $action
          */
         public function __construct($action){
-            $yaml = Yaml::parse(file_get_contents('Others/config/config.yml'));
+            new CreateErrorLog('Action '.$action.' Not Found');
 
-            if($yaml['log']['error'] == 'yes'){
-                new CreateErrorLog('Action '.$action.' Not Found');
-            }
-
-            header("HTTP/1.0 404 Not Found");
             return $this->view('ActionNotFound.html.twig', 'Exception', [ 'action' => $action ]);
         }
     }
