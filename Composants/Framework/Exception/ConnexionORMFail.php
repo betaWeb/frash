dca9dfd1ec915bbbcfd09399dae11b7f40401df1
AlixSperoza <denis.drouin@supinternet.fr>
2016-06-23 14:01:52 +0200

    Update ConnexionORMFail

--- Composants/Framework/Exception/ConnexionORMFail.php
+++ Composants/Framework/Exception/ConnexionORMFail.php
@@ -2,7 +2,6 @@
     namespace Composants\Framework\Exception;
     use Composants\Framework\Controller;
     use Composants\Framework\CreateLog\CreateErrorLog;
-    use Composants\Yaml\Yaml;
 
     /**
      * Class ConnexionORMFail
@@ -13,12 +12,8 @@
          * ConnexionORMFail constructor.
          */
         public function __construct(){
-            $yaml = Yaml::parse(file_get_contents('Others/config/config.yml'));
+            new CreateErrorLog('La connexion à la base de données par l\'ORM n\'a pu être effectuée');
 
-            if($yaml['log']['error'] == 'yes'){
-                new CreateErrorLog('La connexion à la base de données par l\'ORM n\'a pu être effectuée');
-            }
-            
             return $this->view('ConnexionORMFail.html.twig', 'Exception');
         }
     }
