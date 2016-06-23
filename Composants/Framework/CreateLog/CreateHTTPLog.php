2abb163a5e8a69c2c4eb67e9ced8fbb6ac587173
AlixSperoza <denis.drouin@supinternet.fr>
2016-06-23 14:01:04 +0200

    Update CreateHttpLog

--- Composants/Framework/CreateLog/CreateHTTPLog.php
+++ Composants/Framework/CreateLog/CreateHTTPLog.php
@@ -1,5 +1,6 @@
 <?php
     namespace Composants\Framework\CreateLog;
+    use Composants\Yaml\Yaml;
 
     /**
      * Class CreateHTTPLog
@@ -8,11 +9,15 @@
     class CreateHTTPLog{
         /**
          * CreateHTTPLog constructor.
-         * @param $url
+         * @param string $url
          */
         public function __construct($url){
-            $file = fopen('Others/logs/access.log', 'a');
-            fwrite($file, date('d/m/Y à H:i:s').' - IP : '.$_SERVER['REMOTE_ADDR'].' - '.$url."\n");
-            fclose($file);
+            $yaml = Yaml::parse(file_get_contents('Others/config/config.yml'));
+
+            if($yaml['log']['access'] == 'yes'){
+                $file = fopen('Others/logs/access.log', 'a');
+                fwrite($file, date('d/m/Y à H:i:s').' - IP : '.$_SERVER['REMOTE_ADDR'].' - '.$url."\n");
+                fclose($file);
+            }
         }
     }
