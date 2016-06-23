f6b906bb5a1014e6c0ad3d6f23dfa01d0081dbef
AlixSperoza <denis.drouin@supinternet.fr>
2016-06-23 14:03:39 +0200

    Update RouteChargementFail

--- Composants/Framework/Exception/RouteChargementFail.php
+++ Composants/Framework/Exception/RouteChargementFail.php
@@ -2,7 +2,6 @@
     namespace Composants\Framework\Exception;
     use Composants\Framework\Controller;
     use Composants\Framework\CreateLog\CreateErrorLog;
-    use Composants\Yaml\Yaml;
 
     /**
      * Class RouteChargementFail
@@ -11,16 +10,11 @@
     class RouteChargementFail extends Controller{
         /**
          * RouteChargementFail constructor.
-         * @param $route
+         * @param string $route
          */
         public function __construct($route){
-            $yaml = Yaml::parse(file_get_contents('Others/config/config.yml'));
+            new CreateErrorLog('Route '.$route.' Not Found');
 
-            if($yaml['log']['error'] == 'yes'){
-                new CreateErrorLog('Route '.$route.' Not Found');
-            }
-
-            header("HTTP/1.0 404 Not Found");
             return $this->view('RouteNotFound.html.twig', 'Exception', [ 'route' => $route ]);
         }
     }
