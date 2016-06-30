30492bfc4c0084c30943e7abae7030aa4b57c72f
AlixSperoza <denis.drouin@supinternet.fr>
2016-06-30 11:56:32 +0200

    Refonte du Controller

--- Composants/Framework/Controller.php
+++ Composants/Framework/Controller.php
@@ -1,5 +1,5 @@
 <?php
-namespace Composants\Framework;
+    namespace Composants\Framework;
     use Composants\Framework\CreateLog\CreateErrorLog;
     use Composants\Framework\Exception\ConnexionORMFail;
     use Composants\Framework\Exception\TwigChargementTemplateFail;
@@ -14,29 +14,24 @@ namespace Composants\Framework;
         /**
          * @var array
          */
-        private $yaml = [];
+        private static $yaml = [];
 
         /**
          * @var array
          */
-        private $nurl = [];
+        private static $nurl = [];
 
         /**
-         * @var string
+         * @var \PDO
          */
-        protected $bundle = '';
-
-        /**
-         * @var mixed
-         */
-        private $connexion;
+        private static $connexion;
 
         /**
          * Controller constructor.
          */
         public function __construct(){
-            $this->yaml = Yaml::parse(file_get_contents('Others/config/config.yml'));
-            $this->nurl = explode('/', ltrim(Server::getRequestUri(), '/'));
+            self::$yaml = Yaml::parse(file_get_contents('Others/config/config.yml'));
+            self::$nurl = explode('/', ltrim(Server::getRequestUri(), '/'));
         }
 
         /**
@@ -59,32 +54,29 @@ namespace Composants\Framework;
             $twig = new \Twig_Environment($tlf, [ 'cache' => false ]);
 
             $url = new \Twig_SimpleFunction('url', function ($url, $trad = ''){
-                if($this->yaml['env'] == 'local'){
-                    $echo = '/'.$this->nurl['0'].'/';
-
-                    if($trad == 'false' || $this->yaml['traduction']['activate'] != 'yes'){
-                        echo $echo.$url;
+                if('/'.self::$nurl[0] == self::$yaml['prefix']){
+                    if(in_array(self::$nurl[1], self::$yaml['traduction']['available'])){
+                        if($trad === true){
+                            echo '/'.self::$nurl[0].'/'.$url;
+                        }
+                        else{
+                            echo '/'.self::$nurl[0].'/'.self::$nurl[1].'/'.$url;
+                        }
                     }
-                    elseif($this->yaml['traduction']['activate'] == 'yes'){
-                        echo $echo.$this->nurl['1'].'/'.$url;
-                    }
-                }
-                elseif($this->yaml['env'] == 'prod'){
-                    if($trad == 'false' || $this->yaml['traduction']['activate'] != 'yes'){
-                        echo '/'.$url;
-                    }
-                    elseif($this->yaml['traduction']['activate'] == 'yes'){
-                        echo '/'.$this->nurl['0'].'/'.$url;
+                    else{
+                        echo '/'.self::$nurl[0].'/'.$url;
                     }
                 }
             });
 
             $trad = new \Twig_SimpleFunction('trad', function($traduction){
-                if($this->yaml['env'] == 'local'){
-                    $lang = $this->nurl['1'];
-                }
-                elseif($this->yaml['env'] == 'prod'){
-                    $lang = $this->nurl['0'];
+                if('/'.self::$nurl[0] == self::$yaml['prefix']){
+                    if(in_array(self::$nurl[1], self::$yaml['traduction']['available'])){
+                        $lang = self::$nurl[1];
+                    }
+                    else{
+                        $lang = self::$yaml['traduction']['default'];
+                    }
                 }
 
                 $class = 'Traductions\\Trad'.ucfirst($lang);
@@ -105,30 +97,12 @@ namespace Composants\Framework;
         public function redirectToRoute($url){
             $redirect = '';
 
-            if($this->yaml['env'] == 'local'){
-                if($this->yaml['traduction']['activate'] == 'yes'){
-                    if(empty($this->nurl['1'])){
-                        $redirect = $this->nurl['0'].'/'.$this->yaml['traduction']['default'];
-                    }
-                    else{
-                        $redirect = $this->nurl['0'].'/'.$this->nurl['1'];
-                    }
+            if('/'.self::$nurl[0] == self::$yaml['prefix']){
+                if(in_array(self::$nurl[1], self::$yaml['traduction']['available'])){
+                    $redirect = self::$nurl[0].'/'.self::$nurl[1];
                 }
                 else{
-                    $redirect = $this->nurl['0'];
-                }
-            }
-            elseif($this->yaml['env'] == 'prod'){
-                if($this->yaml['traduction']['activate'] == 'yes'){
-                    if(empty($this->nurl['0'])){
-                        $redirect = $this->yaml['traduction']['default'];
-                    }
-                    else{
-                        $redirect = $this->nurl['0'];
-                    }
-                }
-                else{
-                    $redirect = '';
+                    $redirect = self::$nurl[0];
                 }
             }
 
@@ -151,28 +125,20 @@ namespace Composants\Framework;
          * @param string $url
          * @return string
          */
-        public function getUrl($url){
-            if($this->yaml['env'] == 'local'){
-                if($this->yaml['traduction']['activate'] == 'yes' && in_array($this->nurl['1'], $this->yaml['traduction']['available'])){
-                    return '/'.$this->nurl['0'].'/'.$this->nurl['1'].'/'.$url;
+        public static function getUrl($url){
+            if('/'.self::$nurl[0] == self::$yaml['prefix']){
+                if(in_array(self::$nurl[1], self::$yaml['traduction']['available'])){
+                    return '/'.self::$nurl[0].'/'.self::$nurl[1].'/'.$url;
                 }
                 else{
-                    return '/'.$this->nurl['0'].'/'.$url;
-                }
-            }
-            elseif($this->yaml['env'] == 'prod'){
-                if($this->yaml['traduction']['activate'] == 'yes' && in_array($this->nurl['0'], $this->yaml['traduction']['available'])){
-                    return '/'.$this->nurl['0'].'/'.$url;
-                }
-                else{
-                    return '/'.$url;
+                    return '/'.self::$nurl[0].'/'.$url;
                 }
             }
         }
 
         /**
          * @param string $type_form
-         * @param mixed $spec
+         * @param array $spec
          * @return string
          */
         public function createForm($type_form, $spec){
@@ -185,7 +151,7 @@ namespace Composants\Framework;
          * @param string $bundle
          * @return ConnexionORMFail
          */
-        public function initORM($bundle){
+        public static function initORM($bundle){
             $conn = Yaml::parse(file_get_contents('Others/config/database.yml'));
 
             $host = $conn[ $bundle ]['host'];
@@ -196,11 +162,11 @@ namespace Composants\Framework;
 
             try{
                 if($system == 'MySQL'){
-                    $this->connexion = new \PDO('mysql:host='.$host.';dbname='.$dbname.';charset=UTF8;', $username, $password, [ \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC ]);
+                    self::$connexion = new \PDO('mysql:host='.$host.';dbname='.$dbname.';charset=UTF8;', $username, $password, [ \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC ]);
                 }
                 elseif($system == 'PGSQL'){
-                    $this->connexion = new \PDO('pgsql:dbname='.$dbname.';host='.$host, $username, $password);
-                    $this->connexion->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                    self::$connexion = new \PDO('pgsql:dbname='.$dbname.';host='.$host, $username, $password);
+                    self::$connexion->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
                 }
             }
             catch(\Exception $e){
@@ -210,9 +176,9 @@ namespace Composants\Framework;
         }
 
         /**
-         * @return mixed
+         * @return \PDO
          */
-        public function getConnexion(){
-            return $this->connexion;
+        public static function getConnexion(){
+            return self::$connexion;
         }
     }
