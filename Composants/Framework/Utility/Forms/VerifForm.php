ed5996dac8f7163b27e2cf912a2507bbd8105d99
AlixSperoza <denis.drouin@supinternet.fr>
2016-07-07 10:53:28 +0200

    Update class VerifForm

--- Composants/Framework/Utility/Forms/VerifForm.php
+++ Composants/Framework/Utility/Forms/VerifForm.php
@@ -8,38 +8,17 @@
      */
     class VerifForm{
         /**
-         * @var string
-         */
-        private $name_submit;
-
-        /**
-         * @var string
-         */
-        private $csrf;
-
-        /**
-         * @var bool
-         */
-        private $activ_csrf;
-
-        /**
-         * VerifForm constructor.
          * @param string $name_submit
          * @param bool $activ_csrf
          * @param string $csrf
+         * @return bool
          */
-        public function __construct($name_submit, $activ_csrf, $csrf = ''){
-            $this->name_submit = $name_submit;
-            $this->activ_csrf = $activ_csrf;
-            $this->csrf = $csrf;
-
+        public static function valid($name_submit, $activ_csrf, $csrf = ''){
             new Session();
-        }
 
-        public function valid(){
-            if(isset($_POST[ $this->name_submit ])){
-                if($this->activ_csrf){
-                    return (Session::getSession('token') == $this->csrf) ? true : false;
+            if(isset($_POST[ $name_submit ])){
+                if($activ_csrf){
+                    return (Session::getSession('token') == $csrf) ? true : false;
                 }
             }
             else{
