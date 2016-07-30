e6a978b8c385b6ed02e55bdcfd471f4f9c156318
AlixSperoza <denis.drouin@supinternet.fr>
2016-07-30 11:31:07 +0200

    Add file routing_dev.yml and add method to development controllers

--- Composants/Framework/Routing/Router.php
+++ Composants/Framework/Routing/Router.php
@@ -56,6 +56,11 @@
         private static $static = '';
 
         /**
+         * @var string
+         */
+        private static $action = '';
+
+        /**
          * Router constructor.
          * @param string $url
          */
@@ -87,6 +92,17 @@
             array_shift(self::$path);
 
             $racine = 0;
+
+            if(self::$path[0][0].self::$path[0][1] == '__'){
+                $rout_dev = Yaml::parse(file_get_contents('Others/config/config.yml'));
+
+                if(isset($rout_dev[ self::$path[0] ]) && self::$confarr['env'] == 'local'){
+                    self::$route = $rout_dev[ self::$path[0] ]['path'];
+                    self::$action = $rout_dev[ self::$path[0] ]['action'];
+
+                    return self::devController();
+                }
+            }
 
             if(empty(self::$path[0]) && !empty(self::$confarr['racine']['path'])){
                 self::$nb_expl = 1;
@@ -163,7 +179,7 @@
                 }
 
                 Get::set($get);
-                self::returnController();
+                return self::returnController();
             }
             else{
                 return new RouteChargementFail(implode('/', self::$path));
@@ -171,7 +187,7 @@
         }
 
         /**
-         * @return ActionChargementFail|ControllerChargementFail
+         * @return object|ActionChargementFail|ControllerChargementFail
          */
         private static function returnController(){
             list($bundle, $controller, $action) = explode(':', self::$route);
@@ -192,6 +208,19 @@
             }
             elseif(!method_exists($routing, $action)){
                 return new ActionChargementFail($action);
+            }
+        }
+
+        /**
+         * @return object
+         */
+        private static function devController(){
+            $routing = self::$route;
+            $action = self::$action;
+
+            if(file_exists($routing.'.php')){
+                $rout = new $routing;
+                return $rout->$action();
             }
         }
     }
