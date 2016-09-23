d17251399d4dfb426f64f487081f84f94e0e1095
AlixSperoza <denis.drouin@supinternet.fr>
2016-09-23 10:13:41 +0200

    Remove param false in line 15

--- Composants/Framework/Exception/Exception.php
+++ Composants/Framework/Exception/Exception.php
@@ -12,7 +12,7 @@
          * @param string $message
          */
         public function __construct($message){
-            new CreateErrorLog($message, false);
+            new CreateErrorLog($message);
 
             header('HTTP/1.0 404 Not Found', true, 404);
             die();
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # LFW
 Light FrameWork
 
- - <kbd>Framework</kbd> : 0.12.169
+ - <kbd>Framework</kbd> : 0.12.170
 
 Installation
 ----------
