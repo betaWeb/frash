587c784090aaead7f4ac509d1a4298ecacd724bf
AlixSperoza <denis.drouin@supinternet.fr>
2016-09-23 11:22:51 +0200

    Add form to choice bundle

--- Composants/Framework/Systems/SQL/Sql.php
+++ Composants/Framework/Systems/SQL/Sql.php
@@ -1,6 +1,7 @@
 <?php
     namespace Composants\Framework\Systems\SQL;
     use Composants\Framework\DIC\Dic;
+    use Composants\Yaml\Yaml;
 
     /**
      * Class Sql
@@ -13,7 +14,22 @@
             $gets = $dic->open('get');
 
             if(empty($gets->get('get', 0))){
-                return $dic->load('view')->viewDev('sql_no_choice.html.twig', self::PATH);
+                $list_db = Yaml::parse(file_get_contents('Composants/Configuration/database.yml'));
+
+                $array = [];
+                foreach($list_db as $k => $v){
+                    $array[ $k ] = $k;
+                }
+
+                $form = $dic->load('form')->create();
+                return $dic->load('view')->viewDev('sql_no_choice.html.twig', self::PATH, [
+                    'form' => [
+                        'start' => $form->create('StartForm', [ 'method' => 'post', 'action' => $dic->load('getUrl')->url('__sql/connexion/') ]),
+                        'bundle' => $form->create('Select', [ [ 'name' => 'bundle' ], $array ]),
+                        'submit' => $form->create('Submit', [ 'name' => 'submit', 'value' => 'Connexion', 'class' => 'btn btn-default' ]),
+                        'end' => '</form>'
+                    ]
+                ]);
             }
             else{}
         }
