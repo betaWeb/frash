c10157b2cbdf83d64e352bda900b64cdf96e14f2
AlixSperoza <denis.drouin@supinternet.fr>
2016-10-11 17:32:19 +0200

    Modification for preparation of passage in Packagist

--- LFW/Console/listcommand.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $array = [
-        'ORM:addentity:bundle:table     ' => 'Générer une entité',
-        'ORM:createdb                   ' => 'Créer la base de données',
-        'Bundle:generate:nombundle      ' => 'Générer un bundle',
-        'Controller:generate            ' => 'Générer un controller'
-    ];
