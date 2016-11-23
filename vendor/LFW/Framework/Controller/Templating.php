41e5ed4d0912de31214fd84f4b70f4b35524087c
AlixSperoza <denis.drouin@supinternet.fr>
2016-11-23 19:30:12 +0100

    Replace Templating by Template

--- vendor/LFW/Framework/Controller/Templating.php
+++ vendor/LFW/Framework/Controller/Templating.php
@@ -8,10 +8,6 @@
 
 		public function __construct(Dic $dic){
             $this->dic = $dic;
-
-            $gets = $dic->open('get');
-            $this->cache = $gets->get('cache_twig');
-            $this->env = $gets->get('env');
 		}
 
 		public function view($file, $bundle = '', $param = []){
