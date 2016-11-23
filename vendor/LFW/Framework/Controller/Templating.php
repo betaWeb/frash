d1de20811ed9a3de26dcfb0f798ec5c0148d35dd
AlixSperoza <denis.drouin@supinternet.fr>
2016-11-23 19:29:25 +0100

    Replace Templating by Template

--- vendor/LFW/Framework/Controller/Templating.php
+++ vendor/LFW/Framework/Controller/Templating.php
@@ -1,41 +1,17 @@
 <?php
 	namespace LFW\Framework\Controller;
     use LFW\Framework\DIC\Dic;
-	use LFW\Framework\Templating\Loader;
+	use LFW\Framework\Template\Loader;
 
 	class Templating{
-        /**
-         * @var string
-         */
-        private $bundle = '';
-
-        /**
-         * @var string
-         */
-        private $cache = '';
-
-        /**
-         * @var string
-         */
-        private $env = '';
-
-        /**
-         * @var string
-         */
-        private $lang = '';
-
-        /**
-         * @var array
-         */
-        private $nurl = [];
+        private $dic;
 
 		public function __construct(Dic $dic){
+            $this->dic = $dic;
+
             $gets = $dic->open('get');
-            $this->bundle = $gets->get('bundle');
             $this->cache = $gets->get('cache_twig');
             $this->env = $gets->get('env');
-            $this->lang = $gets->get('lang');
-            $this->nurl = explode('/', $gets->get('uri'));
 		}
 
 		public function view($file, $bundle = '', $param = []){
@@ -47,16 +23,9 @@
                 $params = $bundle;
             }
 
-            $list = [
-            	'bundle' => $this->bundle,
-                'cache' => $this->cache,
-                'env' => $this->env,
-                'lang' => $this->lang,
-            	'nurl' => $this->nurl,
-            	'param' => $params
-            ];
-
-            $loader = new Loader($file, $list);
+            $loader = new Loader($file, $params, $this->dic);
             $loader->view();
+
+            return true;
 		}
 	}
