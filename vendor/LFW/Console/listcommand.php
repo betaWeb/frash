d06d08ddd745533663a949fe93163c498a3754c1
AlixSperoza <denis.drouin@supinternet.fr>
2016-11-28 09:08:15 +0100

    Add command for generation of documentation

--- vendor/LFW/Console/listcommand.php
+++ vendor/LFW/Console/listcommand.php
@@ -2,7 +2,7 @@
     $array = [
         'Bundle:generate:nombundle      ' => 'Générer un bundle',
         'Controller:generate            ' => 'Générer un controller',
-        'Documentation:create			' *> 'Générer une documentation',
+        'Documentation:create			' => 'Générer une documentation',
         'ORM:addentity:bundle:table     ' => 'Générer une entité',
         'ORM:createdb                   ' => 'Créer la base de données'
     ];
