208d3940174e17f867fc8b06aa083458625868c5
Alix Speroza <denis.drouin@supinternet.fr>
2016-12-24 22:21:04 +0100

    Update DTE

--- vendor/LFW/Framework/Template/DependTemplEngine.php
+++ vendor/LFW/Framework/Template/DependTemplEngine.php
@@ -1,46 +1,46 @@
 <?php
-	namespace LFW\Framework\Template;
+    namespace LFW\Framework\Template;
 
     /**
      * Class DependTemplEngine
      * @package LFW\Framework\Template
      */
-	class DependTemplEngine{
+    class DependTemplEngine{
         const EXTENSIONS = 'LFW.Framework.Template.Extensions';
 
         /**
          * @var array
          */
-		private $dependencies = [
-			'Bundle' => self::EXTENSIONS.'.Bundle',
-			'Condition' => self::EXTENSIONS.'.ConditionParse',
-			'Escape' => self::EXTENSIONS.'.Escape',
-			'Foreach' => self::EXTENSIONS.'.ForeachParse',
-			'FormatVar' => self::EXTENSIONS.'.FormatVar',
-			'Part' => self::EXTENSIONS.'.Part',
-			'Route' => self::EXTENSIONS.'.Route',
-			'ShowVar' => self::EXTENSIONS.'.ShowVar'
-		];
+        private $dependencies = [
+            'Bundle' => self::EXTENSIONS.'.Bundle',
+            'Condition' => self::EXTENSIONS.'.ConditionParse',
+            'Escape' => self::EXTENSIONS.'.Escape',
+            'Foreach' => self::EXTENSIONS.'.ForeachParse',
+            'FormatVar' => self::EXTENSIONS.'.FormatVar',
+            'Part' => self::EXTENSIONS.'.Part',
+            'Route' => self::EXTENSIONS.'.Route',
+            'ShowVar' => self::EXTENSIONS.'.ShowVar'
+        ];
 
         /**
          * @var array
          */
-		private $open = [];
+        private $open = [];
 
         /**
          * @var array
          */
-		private $params = [];
+        private $params = [];
 
         /**
          * @param string $key
          * @return object
          */
-		public function load(string $key){
-			if(array_key_exists($key, $this->open)){
+        public function load(string $key){
+            if(array_key_exists($key, $this->open)){
                 return $this->open[ $key ];
             }
-			elseif(array_key_exists($key, $this->dependencies)){
+            elseif(array_key_exists($key, $this->dependencies)){
                 $path = str_replace('.', '\\', $this->dependencies[ $key ]);
 
                 $class = new $path($this, $this->params);
@@ -48,20 +48,20 @@
 
                 return $class;
             }
-		}
+        }
 
         /**
          * @param string $name
          * @param mixed $param
          */
-		public function setParams(string $name, $param){
-			$this->params[ $name ] = $param;
-		}
+        public function setParams(string $name, $param){
+            $this->params[ $name ] = $param;
+        }
 
         /**
          * @param string $name
          */
-		public function unsetParams(string $name){
-			unset($this->params[ $name ]);
-		}
-	}
+        public function unsetParams(string $name){
+            unset($this->params[ $name ]);
+        }
+    }
