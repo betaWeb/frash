229a770dcfbf6f65873f092ee1d3bf84bfccf215
Alix Speroza <denis.drouin@supinternet.fr>
2016-12-24 02:14:30 +0100

    Automatisation to find prefix and lang

--- index.php
+++ index.php
@@ -3,8 +3,13 @@
     require_once('vendor/autoload.php');
     use LFW\Framework\DIC\Dic;
     use LFW\Framework\Globals\Server;
+    use LFW\Framework\Routing\Prefix;
 
     $dic = new Dic();
-    $dic->load('get')->set('time_warning', time());
+
+    $gets = $dic->load('get');
+    $gets->set('time_warning', time());
+    $gets->set('prefix', Prefix::define(Server::getScriptName()));
+
     $dic->load('microtime')->setMicrotime('start');
-    $dic->load('router')->routing(Server::getReqUriTrim());
+    $dic->load('router')->routing(ltrim(Server::getRequestUri(), $gets->get('prefix')));
