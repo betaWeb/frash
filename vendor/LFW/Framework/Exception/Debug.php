0f4fc2d880982a43456e93f24f33111cc527d17c
Alix Speroza <denis.drouin@supinternet.fr>
2017-01-04 12:13:38 +0100

    Add a small class to dump

---
+++ vendor/LFW/Framework/Exception/Debug.php
@@ -0,0 +1,12 @@
+<?php
+	namespace LFW\Framework\Exception;
+
+	class Debug{
+		public static function dump($array){
+			print_r($array);
+		}
+
+		public static function predump($array){
+			echo '<pre>'; print_r($array); echo '</pre>';
+		}
+	}
