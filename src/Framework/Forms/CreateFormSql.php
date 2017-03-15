9e50219f2e88e734540156755afdc06afc955f25
Alix Speroza <denis.drouin@supinternet.fr>
2017-03-15 23:15:18 +0100

    Replace LFW by Frash

--- src/Framework/Forms/CreateFormSql.php
+++ src/Framework/Forms/CreateFormSql.php
@@ -1,10 +1,10 @@
 <?php
-namespace LFW\Framework\Forms;
-use LFW\Framework\DIC\Dic;
+namespace Frash\Framework\Forms;
+use Frash\Framework\DIC\Dic;
 
 /**
  * Class CreateFormSql
- * @package LFW\Framework\Forms
+ * @package Frash\Framework\Forms
  */
 class CreateFormSql{
     /**
