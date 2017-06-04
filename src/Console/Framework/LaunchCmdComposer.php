af65556ca86b5d5fe74e263fc2ca9263d171c13d
Alix Speroza <denis.drouin@supinternet.fr>
2017-06-05 00:26:48 +0200

    [Feature] Add define package

--- src/Console/Framework/LaunchCmdComposer.php
+++ src/Console/Framework/LaunchCmdComposer.php
@@ -3,7 +3,7 @@ namespace Frash\Console\Framework;
 use Frash\Console\Answer;
 use Frash\Console\Bundle\CreateBundle;
 use Frash\Console\Framework\Install\{ Configuration, Storage, Traduction };
-use Frash\Framework\FileSystem\Directory;
+use Frash\Framework\FileSystem\{ Directory, File, Json };
 
 /**
  * Class LaunchCmdComposer
@@ -13,32 +13,61 @@ class LaunchCmdComposer
 {
 	public static function work()
 	{
-		$inspecter = Answer::define('Activation de l\'inspecter (yes/no) : ');
-                $cache = Answer::define('Activation du cache de template (yes/no) : ');
-                $default_lang = Answer::define('Langue par défaut (fr) : ');
-                $dispo_lang = Answer::define('Langues disponibles (fr/en/de/...) : ');
-                $bundle = Answer::define('Nom du premier bundle (AppBundle) : ');
+        $package = self::definePackage();
 
-                CreateBundle::verifDirExist();
-                CreateBundle::createDir($bundle);
+        if($package == 'alixsperoza/frash'){
+            $inspecter = Answer::define('Activation de l\'inspecter (yes/no) : ');
+        } else {
+            $inspecter = 'no';
+        }
 
-                Directory::notExistAndCreate('public/');
-                Directory::notExistAndCreate('Traductions/');
+        $cache = Answer::define('Activation du cache de template (yes/no) : ');
+        $default_lang = Answer::define('Langue par défaut (fr) : ');
+        $dispo_lang = Answer::define('Langues disponibles (fr/en/de/...) : ');
 
-                Configuration::preinstall();
-                Configuration::htaccess();
-                Configuration::config($inspecter, $cache, $default_lang, $dispo_lang);
-                Configuration::database($bundle);
-                Configuration::dependencies();
-                Configuration::routing();
-                Configuration::console();
-                Configuration::service();
-                Configuration::testunit();
+        if($package == 'alixsperoza/frash'){
+            $bundle = Answer::define('Nom du premier bundle (AppBundle) : ');
+        } else {
+            $bundle = 'CmsBundle';
+        }
 
-                Storage::preinstall();
-                Storage::cache();
-                Storage::logs();
+        CreateBundle::verifDirExist();
+        CreateBundle::createDir($bundle);
 
-                Traduction::create($dispo_lang);
+        Directory::notExistAndCreate('public/');
+        Directory::notExistAndCreate('Traductions/');
+
+        Configuration::preinstall();
+        Configuration::htaccess();
+        Configuration::config($inspecter, $cache, $default_lang, $dispo_lang);
+        Configuration::database($bundle);
+        Configuration::dependencies();
+        Configuration::routing();
+        Configuration::console();
+        Configuration::service();
+        Configuration::testunit();
+
+        Storage::preinstall();
+        Storage::cache();
+        Storage::logs();
+
+        Traduction::create($dispo_lang);
 	}
+
+    /**
+     * @return string
+     */
+    public static function definePackage(): string
+    {
+        $composer = Json::decode(File::read('composer.json'));
+        $packages = [ 'alixsperoza/frash-cms' ];
+
+        foreach($composer['require'] as $package => $version){
+            if(in_array($package, $packages)){
+                return $package;
+            }
+        }
+
+        return 'alixsperoza/frash';
+    }
 }
