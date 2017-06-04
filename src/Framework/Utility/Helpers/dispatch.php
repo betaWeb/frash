f225179db49ffff0b2ebcf3a0bf81be8ad25f6dd
Alix Speroza <denis.drouin@supinternet.fr>
2017-06-05 01:11:41 +0200

    [Fix] Add param true in json_decode

--- src/Framework/Utility/Helpers/dispatch.php
+++ src/Framework/Utility/Helpers/dispatch.php
@@ -6,7 +6,7 @@ function defineDispatcher()
 	$class = 'Frash\Framework\Dispatch\Dispatcher';
 
 	if($file = File::read('dispatch.json')){
-		$json = json_decode($file);
+		$json = json_decode($file, true);
 
 		if(!empty($json['class'])){
 			$class = $json['class'];
